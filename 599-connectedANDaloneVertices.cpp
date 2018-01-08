b9982d195228360b7663f9a98a2440d27c872286
Pieve <pievetp@gmail.com>
2018-01-08 17:59:28 -0200

    Update 599-connectedANDaloneVertices.cpp

--- 599-connectedANDaloneVertices.cpp
+++ 599-connectedANDaloneVertices.cpp
@@ -1,11 +1,14 @@
-#include<bits/stdc++.h>
+#include <stdio.h>
+#include <bitset>
 
 using namespace std;
+
+const int MAX = 26;
 
 char get_token(){
   char c;
   while(scanf("%c",&c)){
-    if( (c>='A' && c<='Z') || c == '*')
+    if((c >= 'A' && c <= 'Z') || c == '*')
       return c;
   }
   return 0;
@@ -17,29 +20,33 @@ int main(){
 
   while(tc--){
     char a;
-    int num_edges=0;
-    bitset<26> connected;
+    int num_edge = 0;
+    bitset<MAX> connected;
     while(true){
       a = get_token();
-      if(a == '*') break;
-      num_edges++;
-      connected.set(a-'A');
-      connected.set(get_token() - 'A');
+      if(a == '*') break;                // this read just one *
+      num_edge++;
+      connected.set(a - 'A');            // set true for a - 'A' == number
+      connected.set(get_token() - 'A');  // bset.set(pos) makes bset[pos] = 1
     }
+    int num_vertices = 0;
+    int num_acorn = 0;
 
     char s[250];
-    fgets(s,250,stdin); /// ***
     fgets(s,250,stdin);
+    fgets(s,250,stdin);                   //this read the vertices A,B,C...
 
-    int num_vertices=0; int num_acorn=0;
     for(int i=0;s[i];i++){
-      if(s[i]<'A' || s[i]>'Z') continue;
-      if(!connected[s[i]-'A']) num_acorn++;
+      if(s[i] < 'A' || s[i] > 'Z')
+        continue;
       num_vertices++;
+      if(!connected[s[i]-'A'])
+        num_acorn++;
     }
-    int num_tree = num_vertices - num_edges - num_acorn;
-    printf("There are %d tree(s) and %d acorn(s).\n",num_tree,num_acorn);
+    int tree = num_vertices - num_edge - num_acorn;
+    printf("There are %d tree(s) and %d acorn(s).\n",tree,num_acorn);
   }
 
   return 0;
 }
+
