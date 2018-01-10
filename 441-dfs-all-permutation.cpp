44242e311827c3c5da00f8035087b89e71edda5e
Pieve <pievetp@gmail.com>
2018-01-10 19:48:12 -0200

    with iterative solution  too

--- 441-dfs-all-permutation.cpp
+++ 441-dfs-all-permutation.cpp
@@ -35,3 +35,32 @@ int main(){
   
   return 0;
 }
+/* iterative way
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+  freopen("input.txt","r",stdin);
+
+  int k,loto[14];
+  while(scanf("%d",&k), k){
+    for(int i=0;i<k;i++)
+      scanf("%d",&loto[i]);
+
+    for(int i=0;i<k;i++)
+      for(int i2=i+1;i2<k;i2++)
+        for(int i3=i2+1;i3<k;i3++)
+          for(int i4=i3+1;i4<k;i4++)
+            for(int i5=i4+1;i5<k;i5++)
+              for(int i6=i5+1;i6<k;i6++){
+                printf("%d %d %d %d %d %d\n",loto[i],loto[i2],loto[i3],loto[i4],loto[i5],loto[i6]);
+              }
+
+    printf("\n");
+  }
+
+  return 0;
+}
+*/
