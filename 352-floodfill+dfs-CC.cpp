73973de1ff9c1f044c79c96dcb958087635133b6
Pieve <pievetp@gmail.com>
2018-01-11 14:07:33 -0200

    color+dfs+connectcomp

--- 352-floodfill+dfs-CC.cpp
+++ 352-floodfill+dfs-CC.cpp
@@ -1,89 +1,84 @@
-#include <stdio.h>
-#include <algorithm>
-#include <queue>
-#include <iostream>
-#include <cstdio>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-char a[30][30];
-int dimension;
-queue<int> q;
+typedef queue<int> qi;
 
-void dfs(int x,int y){
-  q.push(x);
-  q.push(y);
+int N;
+char img[27][27];
+
+qi q;
+void dfs(int i, int j){
+
+  q.push(i);
+  q.push(j);
 
   while(!q.empty()){
-    x = q.front(); q.pop();
-    y = q.front(); q.pop();
-    
-    if(a[x-1][y] == '1'){
-      q.push(x-1);
-      q.push(y);
-      a[x-1][y]='0';
-    }
-    if(a[x+1][y] == '1'){
-      q.push(x+1);
-      q.push(y);
-      a[x+1][y]='0';
-    }
-    if(a[x][y+1] == '1'){
-      q.push(x);
-      q.push(y+1);
-      a[x][y+1]='0';
-    }
-    if(a[x][y-1] == '1'){
-      q.push(x);
-      q.push(y-1);
-      a[x][y-1]='0';
-    }
+    i = q.front(); q.pop();
+    j = q.front(); q.pop();
 
-    if(a[x-1][y-1] == '1'){
-      q.push(x-1);
-      q.push(y-1);
-      a[x-1][y-1]='0';
+    if(img[i-1][j] == '1'){
+      q.push(i-1);
+      q.push(j);
+      img[i-1][j] = '0';
     }
-    if(a[x+1][y+1] == '1'){
-      q.push(x+1);
-      q.push(y+1);
-      a[x+1][y+1]='0';
+    if(img[i+1][j] == '1'){
+      q.push(i+1);
+      q.push(j);
+      img[i+1][j] = '0';
     }
-    if(a[x-1][y+1] == '1'){
-      q.push(x-1);
-      q.push(y+1);
-      a[x-1][y+1]='0';
+    if(img[i][j-1] == '1'){
+      q.push(i);
+      q.push(j-1);
+      img[i][j-1] = '0';
     }
-    if(a[x+1][y-1] == '1'){
-      q.push(x+1);
-      q.push(y-1);
-      a[x+1][y-1]='0';
+    if(img[i][j+1] == '1'){
+      q.push(i);
+      q.push(j+1);
+      img[i][j+1] = '0';
     }
-
+    if(img[i+1][j+1] == '1'){
+      q.push(i+1);
+      q.push(j+1);
+      img[i+1][j+1] = '0';
+    }
+    if(img[i-1][j-1] == '1'){
+      q.push(i-1);
+      q.push(j-1);
+      img[i-1][j-1] = '0';
+    }
+    if(img[i-1][j+1] == '1'){
+      q.push(i-1);
+      q.push(j+1);
+      img[i-1][j+1] = '0';
+    }
+    if(img[i+1][j-1] == '1'){
+      q.push(i+1);
+      q.push(j-1);
+      img[i+1][j-1] = '0';
+    }
   }
 }
 
 int main() {
    
-//  freopen("input.txt","r",stdin);
- 
-  int cnt=0;
-  while(scanf("%d",&dimension) != EOF){
+   //freopen("input.txt","r",stdin);
+
+  int num_img=1;
+  while(scanf("%d",&N) != EOF){
     getchar();
 
-    for(int i=0;i<dimension;i++)
-      scanf("%s",a[i]);
+    for(int i=0;i<N;i++)
+      scanf("%s",img[i]);
 
-    int comp=0;
-    for(int i=0;i<dimension;i++)
-      for(int j=0;j<dimension;j++)
-        if(a[i][j] == '1'){
-          comp++;
+    int war=0;
+    for(int i=0;i<N;i++)
+      for(int j=0;j<N;j++)
+        if(img[i][j] == '1'){
           dfs(i,j);
+          war++;
         }
-
-    printf("Image number %d contains %d war eagles.\n",++cnt,comp);
-  
+    printf("Image number %d contains %d war eagles.\n", num_img++,war);
   }
   return 0;
 }
