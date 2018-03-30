7145fade92c2c4890497e33b5836c54a0f381c6e
Pieve <pievetp@gmail.com>
2018-03-30 12:36:22 -0300

    equalize numbers

--- zRandom/MaratonaProgramacao/Lista2/URI 1220 - The Trip.cpp
+++ zRandom/MaratonaProgramacao/Lista2/URI 1220 - The Trip.cpp
@@ -1,3 +1,5 @@
+//https://www.urionlinejudge.com.br/judge/en/problems/view/1220
+
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -15,19 +17,23 @@ int main(){
 
         for(int i = 0; i < n; i++){
             scanf("%d.%d", &x, &y);
+            
+            //precision
             a[i] = x*100 + y;
 
             total += a[i];
         }
 
-        total = ((double)total)/n;
+        total = ((double)total)/n;   // how much each person should to pay
 
         double maior = 0, menor = 0;
 
+        
+        // find sum high diff to equalize the numbers
         for(int i = 0; i < n; i++){
             
             if(a[i] < total){
-                menor += ((int)(total - a[i]))/100.0;
+                menor += ((int)(total - a[i]))/100.0;   
             }
             else{
                 maior += ((int)(a[i] - total))/100.0;
